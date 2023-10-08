5474fa5e661ef38a889d8e842f158c448f469a88
barinder <barindersingh1997@gmail.com>
2023-10-08 16:19:15 +0530

    temp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import './Home.css'
 import React, { useEffect, useState } from 'react'
-import data from "./data.json" 
+import data from "./data.json"
 import imging from './IMG_9971.jpg'
 import Header from './Header';
 import Footer from './Footer';
@@ -35,8 +35,8 @@ export default function Home() {
     const filtered = data.filter(item =>
       (item.name && item.name.toLowerCase().includes(lowercaseQuery)) ||
       (item.title && item.title.toLowerCase().includes(lowercaseQuery))
-   );
-   
+    );
+
     setFilteredData(filtered);
   }, [searchQuery]);
 
@@ -71,15 +71,16 @@ export default function Home() {
   return (
     <div>
       <Header searchbar={true} setSearchQuery={setSearchQuery} />
-      <Alert/>
-    <div className='container'>
-        {/* <img style={{"height":"160px"}} src={imging}/>
+      <Alert />
+      {!searchQuery ? <>
+        <div className='container'>
+          {/* <img style={{"height":"160px"}} src={imging}/>
         {data.name}
         {data.title}
         {data.map((items)=>{
-            return (
-                <div className='container'key={items.name}>
-                    <img style={{"height":"80px"}} src={items.image}/>
+          return (
+            <div className='container'key={items.name}>
+            <img style={{"height":"80px"}} src={items.image}/>
 
                 <div className='container'>{items.name}</div>
                 
@@ -89,17 +90,17 @@ export default function Home() {
                 </div>
                 )
         })} */}
-        <div className='container'>
-          <div className='imgelogo'>
-        <img className='mainlogo' src={imging}/>
+          <div className='container'>
+            <div className='imgelogo'>
+              <img className='mainlogo' src={imging} />
+
+            </div>
 
           </div>
-
-        </div>
-        <br></br><br></br>
-        <div style={{"textAlign":"center","marginTop":"10px","marginBottom":"20px"}}>Welcome to BsCustomz - Your Ultimate Destination for Automotive Excellence! 🚀<br></br>
-        Join our community of passionate auto enthusiasts and gearheads, and let's take your automotive experience to the next level! Start your journey today. 🏁
-{/* 
+          <br></br><br></br>
+          <div style={{ "textAlign": "center", "marginTop": "10px", "marginBottom": "20px" }}>Welcome to BsCustomz - Your Ultimate Destination for Automotive Excellence! 🚀<br></br>
+            Join our community of passionate auto enthusiasts and gearheads, and let's take your automotive experience to the next level! Start your journey today. 🏁
+            {/* 
 Rev up your automotive journey with us and explore a world of possibilities. From top-notch performance parts to eye-catching custom stickers, LED bulbs that light up the road, and all the mods your ride craves – we've got it all under one roof.
 <br></br>
 🔧 Unleash Your Ride's Potential: Upgrade your vehicle's performance with our premium parts and expert guidance.
@@ -113,28 +114,28 @@ Rev up your automotive journey with us and explore a world of possibilities. Fro
 <br></br>
 Join our community of passionate auto enthusiasts and gearheads, and let's take your automotive experience to the next level! Start your journey today. 🏁
 <br></br> */}
-</div>
-<br></br>
-<br></br>
-<h4 style={{"textAlign":"center","color":"red","opacity":"60%"}}>Categories</h4>
-{
-         uniqueCategories.map((category) => (
-          <ul>
-            
-          <li key={category}>{category}</li>
-          </ul>
-            
-              
-            
-        ))
-      }     
-<div>
-{/* <br></br> */}
-<br></br>
-{/* <br></br> */}
-</div>
-<h4 style={{"textAlign":"center","color":"red","opacity":"60%"}}>Our Products</h4>
-         {/* {Object.keys(groupedProducts).map((category) => (
+          </div>
+          <br></br>
+          <br></br>
+          <h4 style={{ "textAlign": "center", "color": "red", "opacity": "60%" }}>Categories</h4>
+          {
+            uniqueCategories.map((category) => (
+              <ul>
+
+                <li key={category}>{category}</li>
+              </ul>
+
+
+
+            ))
+          }
+          <div>
+            {/* <br></br> */}
+            <br></br>
+            {/* <br></br> */}
+          </div>
+          <h4 style={{ "textAlign": "center", "color": "red", "opacity": "60%" }}>Our Products</h4>
+          {/* {Object.keys(groupedProducts).map((category) => (
              <div key={category}>
                 <br></br>
                 <br></br>
@@ -158,10 +159,10 @@ Join our community of passionate auto enthusiasts and gearheads, and let's take 
           </div>
         </div>
       ))} */}
-      
 
-<div>
-      {/* {Object.entries(groupedProducts).map(([category, items]) => (
+
+          {/* <div> */}
+            {/* {Object.entries(groupedProducts).map(([category, items]) => (
         <div key={category}>
           <h2>{category}</h2>
           <ul>
@@ -176,51 +177,119 @@ Join our community of passionate auto enthusiasts and gearheads, and let's take 
         </div>
       ))} */}
 
-<div>
-      {Object.entries(groupedProducts).map(([category, items]) => {
-        const filteredItems = items.filter(
-          (item) =>
-            item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            category.toLowerCase().includes(searchQuery.toLowerCase())
-        );
-
-        if (filteredItems.length > 0) {
-          return (
-            <div key={category}>
-              <h2 className='container-fluid'>{category}</h2>
-              <hr/>
-              <br></br>
-                <div className='product-container'>
-                <div className='product-list'>
-                {filteredItems.map((item) => (
-                  <>
-                  {/* <div key={item.name} className='product' onClick={()=>handleProductClick(item)}> */}
-                  <Link className='itemslink' key={item.name} to={`/product/${item.name}`}>
-                  <div key={item.name} className='product'>
-                  <img key={item.image} style={{"height":"80px" , "display":"flex" , "margin":"auto"}} src={`./${item.image}`} alt={item.title} />
-                  <p style={{"textAlign":"center"}} >{item.name}</p>
-                <p  style={{"textAlign":"center"}} >{item.title}</p>  
-                  
-                  </div>
-                  </Link>
-                  
-                  </>
-                  ))}
-                </div>
-                </div>
-              
-            </div>
-          );
-        }
-
-        return null;
-      })}
-    </div>
-    </div>
+            <div>
 
 
-    </div>
-      <Footer/>
+
+
+
+
+              {
+
+
+                Object.entries(groupedProducts).map(([category, items]) => {
+                  const filteredItems = items.filter(
+                    (item) =>
+                      item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                      category.toLowerCase().includes(searchQuery.toLowerCase())
+                  );
+
+                  if (filteredItems.length > 0) {
+                    return (
+                      <div key={category}>
+                        <h2 className='container-fluid'>{category}</h2>
+                        <hr />
+                        <br></br>
+                        <div className='product-container'>
+                          <div className='product-list'>
+                            {filteredItems.map((item) => (
+                              <>
+                                {/* <div key={item.name} className='product' onClick={()=>handleProductClick(item)}> */}
+                                <Link className='itemslink' key={item.name} to={`/product/${item.name}`}>
+                                  <div key={item.name} className='product'>
+                                    <img key={item.image} style={{ "height": "80px", "display": "flex", "margin": "auto" }} src={`./${item.image}`} alt={item.title} />
+                                    <p style={{ "textAlign": "center" }} >{item.name}</p>
+                                    <p style={{ "textAlign": "center" }} >{item.title}</p>
+
+                                  </div>
+                                </Link>
+
+                              </>
+                            ))}
+                          </div>
+                        </div>
+
+                      </div>
+
+                    );
+
+                  }
+                  return null;
+                })}
+
+            {/* </div> */}
+          </div>
+
+
+        </div>
+
+      </> 
+
+     :
+      <>
+
+      {   Object.entries(groupedProducts).map(([category, items]) => {
+         const filteredItems = items.filter(
+           (item) =>
+             item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+             category.toLowerCase().includes(searchQuery.toLowerCase()));
+      
+         if (filteredItems.length > 0) {
+           return (<div>
+             <br></br>
+             <h4 style={{ width: "96%", marginLeft: "auto", marginRight: "auto", textAlign: "center" }} className='category'>Showing results for {searchQuery}</h4>
+             <br></br>
+             <div key={category}>
+               <h2 className='container-fluid'>{category}</h2>
+               <hr />
+               <br></br>
+               <div className='product-container'>
+                 <div className='product-list'>
+                   {filteredItems.map((item) => (
+                     <div>
+                       {/* <div key={item.name} className='product' onClick={()=>handleProductClick(item)}> */}
+                       <Link className='itemslink' key={item.name} to={`/product/${item.name}`}>
+                         
+                         <div key={item.name} className='product'>
+                           <img key={item.image} style={{ "height": "80px", "display": "flex", "margin": "auto" }} src={`./${item.image}`} alt={item.title} />
+                           <p style={{ "textAlign": "center" }} >{item.name}</p>
+                           <p style={{ "textAlign": "center" }} >{item.title}</p>
+                         </div>
+      
+                       </Link>
+      
+                       </div>
+                   ))}
+                 </div>
+               </div>
+              </div>
+             </div>
+           );
+         }
+         
+        
+         return null;
+       })}
+
+      </>
+      
+      }
+
+      <Footer />
     </div>
   )
 }
+
+
+
+
---
+++ src/components/gome.jsx
@@ -0,0 +1,60 @@
+
+
+
+
+
+{/* {searchQuery?<> */}
+
+// {   Object.entries(groupedProducts).map(([category, items]) => {
+
+    //      const filteredItems = items.filter(
+    //        (item) =>
+    //          item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    //          category.toLowerCase().includes(searchQuery.toLowerCase()));
+    
+    //      if (filteredItems.length > 0) {
+    //        return (<div>
+    //          <br></br>
+    //          <h4 style={{ width: "96%", marginLeft: "auto", marginRight: "auto", textAlign: "center" }} className='category'>Showing results for {searchQuery}</h4>
+    //          <br></br>
+    //          <div key={category}>
+    //            <h2 className='container-fluid'>{category}</h2>
+    //            <hr />
+    //            <br></br>
+    //            <div className='product-container'>
+    //              <div className='product-list'>
+    //                {filteredItems.map((item) => (
+    //                  <div>
+    //                    {/* <div key={item.name} className='product' onClick={()=>handleProductClick(item)}> */}
+    //                    <Link className='itemslink' key={item.name} to={`/product/${item.name}`}>
+                         
+    //                      <div key={item.name} className='product'>
+    //                        <img key={item.image} style={{ "height": "80px", "display": "flex", "margin": "auto" }} src={`./${item.image}`} alt={item.title} />
+    //                        <p style={{ "textAlign": "center" }} >{item.name}</p>
+    //                        <p style={{ "textAlign": "center" }} >{item.title}</p>
+    //                      </div>
+    
+    //                    </Link>
+    
+    //                    </div>
+    //                ))}
+    //              </div>
+    //            </div>
+    //           </div>
+    //          </div>
+    //        );
+    //      }
+    //      return null;
+    //    })
+       
+    //  }
+    // </>  :<>(<p style={{ background: "#fbfbfb", width: "96%", marginLeft: "auto", marginRight: "auto", textAlign: "center" }}>No Results Found for {searchQuery}!!</p>
+    //    <img style={{ width: "100%" }} src='./imgss/no_results_found.png'></img>
+    //  )</>
+    //  }
+    
+    
+    
+    
+    
+    
